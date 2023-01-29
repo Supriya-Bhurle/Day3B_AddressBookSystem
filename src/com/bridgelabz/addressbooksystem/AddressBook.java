e3bc8665d2e31d85c145083e2376adaa6a818b6b
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-01-29 18:50:08 +0530

    Added : Ability to edit existing contacts

--- src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -3,23 +3,72 @@ package com.bridgelabz.addressbooksystem;
 import java.util.Scanner;
 
 public class AddressBook {
-    public void addContact() {
-        Contact contactPerson = new Contact();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the details of contact person");
-        System.out.print("Enter first name:");
-        contactPerson.setName(sc.next());
-        System.out.print("Enter Last name:");
-        contactPerson.setLastName(sc.next());
-        System.out.println("Enter the Address : ");
-        contactPerson.setAddress(sc.next());
-        System.out.println("Enter the City : ");
-        contactPerson.setCity(sc.next());
-        System.out.println("Enter the State : ");
-        contactPerson.setState(sc.next());
-        System.out.println("Enter the ZipCode : ");
-        contactPerson.setZipCode(sc.next());
-        System.out.println("Enter the Mobile no : ");
-        contactPerson.setPhoneNo(sc.next());
+    Scanner sc = new Scanner(System.in);
+    Contact contacts = new Contact();
+    public void addNewContact() {
+        System.out.println("----------------------------------");
+        System.out.println("Enter the Contact Details :-");
+        System.out.println("Enter the First Name :");
+        contacts.setFirstname(sc.next());
+        System.out.println("Enter the Last Name :");
+        contacts.setLastname(sc.next());
+        System.out.println("Enter the Address :");
+        contacts.setAddress(sc.next());
+        System.out.println("Enter the City :");
+        contacts.setCity(sc.next());
+        System.out.println("Enter the State :");
+        contacts.setState(sc.next());
+        System.out.println("Enter the PIN Code :");
+        contacts.setPin(sc.nextLong());
+        System.out.println("Enter the Phone Number :");
+        contacts.setPhoneNumber(sc.nextLong());
+        System.out.println("Enter the EMail ID :");
+        contacts.setEmail(sc.next());
+        System.out.println("Contacts Added Successfully------!!!");
+        System.out.println("-----------------------------------------");
     }
+    public void displayContact() {
+        System.out.println("--------------------------------------------");
+        System.out.println("Contact Details :-");
+        System.out.println("Name : "+contacts.getFirstname()+" "+contacts.getLastname());
+        System.out.println("Address : "+contacts.getAddress());
+        System.out.println("City : "+contacts.getCity());
+        System.out.println("State : "+contacts.getState());
+        System.out.println("PIN Code : "+contacts.getPin());
+        System.out.println("Phone Number : "+contacts.getPhoneNumber());
+        System.out.println("EMail ID : "+contacts.getEmail());
+        System.out.println("---------------------------------------------");
+    }
+    public void editContact() {
+        //Get First Name to Edit the Contact
+        System.out.println("Enter the First Name : ");
+        String firstName = sc.next();
+
+        //check if the Given User with First Name
+        if(!firstName.equals(contacts.getFirstname())) {
+            System.out.println("The Entered Contact Name is Not Available in Address Book");
+        } else {
+            System.out.println("Match Found--------!!!!");
+            System.out.println("-----------------------");
+            System.out.println("Enter the First Name :");
+            contacts.setFirstname(sc.next());
+            System.out.println("Enter the Last Name :");
+            contacts.setLastname(sc.next());
+            System.out.println("Enter the Address :");
+            contacts.setAddress(sc.next());
+            System.out.println("Enter the City :");
+            contacts.setCity(sc.next());
+            System.out.println("Enter the State :");
+            contacts.setState(sc.next());
+            System.out.println("Enter the Zip Code :");
+            contacts.setPin(sc.nextLong());
+            System.out.println("Enter the Phone Number :");
+            contacts.setPhoneNumber(sc.nextLong());
+            System.out.println("Enter the EMail ID :");
+            contacts.setEmail(sc.next());
+            System.out.println("-------------------------------");
+            System.out.println("Contact Update Successfully-----!!");
+        }
+    }
+
 }
